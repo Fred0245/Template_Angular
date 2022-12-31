7df3a1274766efb5215dd518a8f644cd634a7c14
Fred0245 <fredericdjaoura@gmail.com>
2022-12-31 11:28:50 +0100

    template angular intégré

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicationComponent } from './application/application.component';
+import { DashboardComponent } from './application/dashboard/dashboard.component';
+import { TestComponent } from './application/test/test.component';
+import { InscriptionComponent } from './authentification/inscription/inscription.component';
+import { LoginComponent } from './authentification/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'inscription',component:InscriptionComponent},
+  {path:'application',component:ApplicationComponent,
+  children:[
+    {path:'dasboard',component:DashboardComponent},
+    {path:'test',component:TestComponent},
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentification/login/login.component';
+import { InscriptionComponent } from './authentification/inscription/inscription.component';
+import { MenuComponent } from './application/menu/menu.component';
+import { NavigationComponent } from './application/navigation/navigation.component';
+import { FooterComponent } from './application/footer/footer.component';
+import { HeaderComponent } from './application/header/header.component';
+import { ApplicationComponent } from './application/application.component';
+import { DashboardComponent } from './application/dashboard/dashboard.component';
+import { TestComponent } from './application/test/test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationComponent,
+    LoginComponent,
+    InscriptionComponent,
+    MenuComponent,
+    NavigationComponent,
+    FooterComponent,
+    HeaderComponent,
+    DashboardComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
